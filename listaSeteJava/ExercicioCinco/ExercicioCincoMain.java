d73561d7fbb1ed0a8081558065e00e39174cbff6
Maria Clara Marques Lino <129996963+Maclalino@users.noreply.github.com>
2023-05-27 17:23:39 -0300

    Add files via upload

---
+++ listaSeteJava/ExercicioCinco/ExercicioCincoMain.java
@@ -0,0 +1,13 @@
+package ExercicioCinco;
+import java.util.Scanner;
+public class ExercicioCincoMain {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        ExercicioCincoFuncao exercicioCincoFuncao = new ExercicioCincoFuncao();
+
+        System.out.println("Digite seu telefone:");
+        exercicioCincoFuncao.setNumero(scanner.nextLine());
+
+        exercicioCincoFuncao.validarCorrigirNumero();
+    }
+}
---
+++ listaSeteJava/ExercicioTres/ExercicioTresFuncao.java
@@ -0,0 +1,15 @@
+package ExercicioTres;
+
+public class ExercicioTresFuncao {
+    private String cpf;
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+    
+    
+}
